4eb8704d805f13b2f0f3294c5697844279549347
roger.barriga <roger.barriga@scribsoft.com>
2025-05-31 10:02:46 -0500

    Fixing dependency error in integration test

--- tests/integration/test_insert_category.py
+++ tests/integration/test_insert_category.py
@@ -2,12 +2,15 @@ import pytest
 
 from src.dao.category_dao import CategoryDAO
 from src.models.new_category_model import NewCategoryModel
+from src.dao.dependencies import get_db
+from fastapi import Depends
+from sqlalchemy.orm import Session
 
 @pytest.mark.asyncio
-async def test_insert_category():
+async def test_insert_category(db: Session = Depends(get_db)):
     category_name = "CATEGORY 1000"
     
-    category_dao = CategoryDAO()
+    category_dao = CategoryDAO(db)
     result = await category_dao.create_category(NewCategoryModel(name=category_name))
     category_id = result.id
     
